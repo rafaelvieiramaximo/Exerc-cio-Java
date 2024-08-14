ef983340f44d78c2b4258427aab19bf91070d08c
Rafael <rafavmaximo@gmail.com>
2024-08-14 12:22:45 -0300

    :sparkles:feat: Adicionando lógica de switch

--- Média/Menu.java
+++ Média/Menu.java
@@ -4,35 +4,69 @@ import java.util.Scanner;
 
 public class Menu {
 
-    public static void main (String [] args){
+    public static void main(String[] args) {
 
-    System.out.println("Olá, digite as notas do menino");
+        System.out.println("Bem vindo ao programa que cálculo de notas...");
 
-    Scanner scanner = new Scanner(System.in);
+        int outro;
 
-    Notas notas = new Notas();
+        do {
 
-    System.out.println("Nota da primeira Prova:");
-    notas.setNtPrv1(scanner.nextDouble());
+            System.out.println("Olá, digite as notas do menino");
 
-    System.out.println("Nota da Segunda Prova:");
-    notas.setNtPrv2(scanner.nextDouble());
+            Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Nota do Primeiro Trabalho:");
-    notas.setNtTrab1(scanner.nextDouble()); 
+            Notas notas = new Notas();
 
-    System.out.println("Nota do Segundo Trabalho:");
-    notas.setNtTrab2(scanner.nextDouble());
+            int opcao;
+            do {
 
-    scanner.close();
+                System.out.println("Nota da primeira Prova:");
+                notas.setNtPrv1(scanner.nextDouble());
 
-    System.out.println(String.format("Média das provas: %.2f", notas.calcMediaProva()));
-    System.out.println(String.format("Média dos trabalhos: %.2f", notas.calcMediaTrab()));
-    System.out.println(String.format("Média Final: %.2f", notas.calcMediaFinal()));
+                System.out.println("Nota da Segunda Prova:");
+                notas.setNtPrv2(scanner.nextDouble());
 
+                System.out.println("Nota do Primeiro Trabalho:");
+                notas.setNtTrab1(scanner.nextDouble());
+
+                System.out.println("Nota do Segundo Trabalho:");
+                notas.setNtTrab2(scanner.nextDouble());
+
+                scanner.close();
+
+                System.out.println("\nEscolha qual média você deseja ver:");
+                System.out.println("1. Média das Provas");
+                System.out.println("2. Média dos trabalhos");
+                System.out.println("3. Média Final");
+                System.out.println("4. Sair\n");
+                opcao = scanner.nextInt();
+
+                switch(opcao){
+                    case 1:
+                    System.out.println(String.format("Média das provas: %.2f", notas.calcMediaProva()));
+                    break;
+                    case 2:
+                    System.out.println(String.format("Média dos trabalhos: %.2f", notas.calcMediaTrab()));
+                    break;
+                    case 3:
+                    System.out.println(String.format("Média Final: %.2f", notas.calcMediaFinal()));
+                    break;
+                    case 4:
+                    System.out.println("Saindo...");
+                    break;
+                    default:
+                    System.out.println("Opção inválida. Tente novamente.");
+                }
+
+            } while (opcao != 4);
+
+            System.out.println("\nDeseja calcular outra média?");
+            System.out.println("1. Sim");
+            System.out.println("2. Não");
+            outro = scanner.nextInt();
+
+        } while (outro != 2);
     }
 
-    
-
-    
 }
